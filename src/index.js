6a3bc157a7433d3c473ddffa8529c35a06b6373f
Jarred Kenny <jarred.kenny@serversitters.com>
2017-03-04 18:04:48 -0400

    stuffz

--- src/index.js
+++ src/index.js
@@ -265,7 +265,7 @@ class SpaceBattles {
     if(this.boss.health < 40 && !this.boss.angry) {
       this.boss.decorations = [];
       this.boss.angry = true;
-      boss.addDecoration('../assets/boss/boss_damaged.png', { x: -25, y: -25 }, { x: boss.size.x+50, y: boss.size.y+50 });
+      this.boss.addDecoration('../assets/boss/boss_damaged.png', { x: -25, y: -25 }, { x: boss.size.x+50, y: boss.size.y+50 });
     }
 }
 
